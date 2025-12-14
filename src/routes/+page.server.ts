eb34bc5f931c636db1cc585ee082828236d224e2
asarmichil <asarmichil@gmail.com>
2025-12-13 23:31:22 -0800

    fix: remove prerender false

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,6 @@ import { HomezoneSchema } from "../types/evo"
 import type { PageServerLoad } from "./$types"
 import { extractParameters } from "../lib/url-params"
 
-export const prerender = false
-
 export const load: PageServerLoad = async ({ url }) => {
   const homezonesParsed = HomezoneSchema.array().parse(homezones)
   const urlParams = extractParameters(url)
