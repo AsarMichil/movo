e43769b0e8369536067c3a1410d9e0420b8b3a18
asarmichil <asarmichil@gmail.com>
2025-12-13 23:37:29 -0800

    prerender and search params

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,15 @@
-import homezones from "../data/evo/homezones.json"
-import { HomezoneSchema } from "../types/evo"
-import type { PageServerLoad } from "./$types"
-import { extractParameters } from "../lib/url-params"
+import homezones from "../data/evo/homezones.json";
+import { HomezoneSchema } from "../types/evo";
+import type { PageServerLoad } from "./$types";
+import { extractParameters } from "../lib/url-params";
+export const prerender = false;
 
 export const load: PageServerLoad = async ({ url }) => {
-  const homezonesParsed = HomezoneSchema.array().parse(homezones)
-  const urlParams = extractParameters(url)
+  const homezonesParsed = HomezoneSchema.array().parse(homezones);
+  const urlParams = extractParameters(url);
 
   return {
     homezones: homezonesParsed,
     urlParams,
-  }
-}
+  };
+};
